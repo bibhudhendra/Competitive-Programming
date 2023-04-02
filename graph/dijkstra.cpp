ca91e0fecc9fa637bc971934756346f0bde634e9
Bibhudhendra Pati <bibhudh@amazon.com>
2023-04-02 08:07:47 +0530

    Updated dijkstra

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file dijkstra.cpp
+ * @author bibhudhendra (patibibhudhendra@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+/*
+Problem
+1. Find sortest distance to all the vertext from a given source vertex
+*/
+
+/*
+Drawbacks
+1. Will not work for -ve edge
+*/
+
+/*
+Time Complexity
+1. Using Heap/PriorityQueue O(Vlog(V+E))
+*/
+
+/*
+Code Snippet Using Heap/PriorityQueue
+*/
+
+
+
+
